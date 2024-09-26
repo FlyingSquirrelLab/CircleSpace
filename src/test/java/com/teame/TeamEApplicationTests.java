618115c83f6999172d875e6e259a87ed47ff2117
Johnny Bae <theobsh@gmail.com>
2024-09-26 13:01:27 +0900

    init commit

---
+++ src/test/java/com/teame/TeamEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamEApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
