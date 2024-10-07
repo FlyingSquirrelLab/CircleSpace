da951698ec97d41b1ebd18cec26b402ce5a24e3f
Johnny Bae <theobsh@gmail.com>
2024-10-07 13:05:01 +0900

    created Member, Club

---
+++ src/main/java/com/teame/club/JoinInfo.java
@@ -0,0 +1,9 @@
+package com.teamE.club;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class JoinInfo {
+
+
+}
--- src/main/java/com/teame/config/WebController.java
+++ src/main/java/com/teame/config/WebController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // 리액트 새로고침시 라우터 오류 핸들러
-
 @Controller
 public class WebController implements ErrorController {
   @GetMapping({"/", "/error"})
