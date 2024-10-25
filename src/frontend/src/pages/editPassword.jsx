9a955b8818dd3410b31ff1167ea5f1bb24c42bf7
Johnny Bae <theobsh@gmail.com>
2024-10-25 14:08:54 +0900

    fix : minor  css import fix

--- src/frontend/src/pages/editPassword.jsx
+++ src/frontend/src/pages/editPassword.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import axiosInstance from "../axiosInstance.jsx";
-import './Register.css'
+import './register.css'
 
 const EditPassword = () => {
 
