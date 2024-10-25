b001de8e3dce1739050db89b02c40fa183187656
Johnny Bae <theobsh@gmail.com>
2024-10-25 14:08:54 +0900

    fix : minor  css import fix

--- src/frontend/src/pages/editUserInfo.jsx
+++ src/frontend/src/pages/editUserInfo.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import axiosInstance from "../axiosInstance.jsx";
-import './Register.css'
+import './register.css'
 
 const EditUserInfo = () => {
 
