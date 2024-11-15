bd011412d90731e2f1d68f7e8471877bbb9435d7
GK <weeeeek2@naver.com>
2024-11-15 16:29:00 +0900

    fix: Add Daily Updates fetch

--- src/main/java/com/teame/promo/PromoController.java
+++ src/main/java/com/teame/promo/PromoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,27 +19,35 @@ public class PromoController {
 
     private final PromoService promoService;
     private final PromoRepository promoRepository;
+    private final PromoDataParser promoDataParser;
 
     @Autowired
-    public PromoController(PromoService promoService, PromoRepository promoRepository){
+    public PromoController(PromoService promoService, PromoRepository promoRepository, PromoDataParser promoDataParser){
         this.promoService = promoService;
         this.promoRepository = promoRepository;
+        this.promoDataParser = promoDataParser;
     }
 
-    @GetMapping("/api/promo/fetchAll")
+    @GetMapping("/api/daily-up/fetchAll")
     public List<PromoDTO> fetchAllPromos() {
         List<Promo> promos = promoRepository.findAll();
         return promos.stream()
                 .map(promoService::setPromoDTO)
                 .collect(Collectors.toList());
     }
+    @GetMapping("/api/daily-up/fetchById/{promoId}")
+    public ResponseEntity<?> fetchPromoByIdAPI(@PathVariable Long promoId) {
+        return promoService.fetchPromoById(promoId);
+    }
+
+
 
     // Manual로 데이터 파싱 수행
     @GetMapping("/api/parse")
     public ResponseEntity<?> fetchParseData()
     {
         try {
-            promoService.runSeleniumTask();
+            promoDataParser.runSeleniumTask();
             return ResponseEntity.ok("");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
