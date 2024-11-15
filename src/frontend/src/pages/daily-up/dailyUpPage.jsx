a2a45a8f0ff70ebf63962f809ba0671cccd9b7f4
GK <weeeeek2@naver.com>
2024-11-15 16:58:33 +0900

    fix: Add PagedModel for Daily Update

--- src/frontend/src/pages/daily-up/dailyUpPage.jsx
+++ src/frontend/src/pages/daily-up/dailyUpPage.jsx
@@ -7,7 +7,7 @@ import PageController from "../../components/pageController.jsx";
 const DailyUpPage = ({formatDate}) => {
 
   const nav = useNavigate();
-  const size = 2;
+  const size = 10;
   const [page, setPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [dailyUpList, setDailyUpList] = useState([]);
@@ -15,10 +15,11 @@ const DailyUpPage = ({formatDate}) => {
   useEffect(() => {
     const fetchDailyUpdates = async () => {
       try {
-        const response = await axios.get(`/api/daily-up/fetchAll`);
+        const response = await axios.get(`/api/daily-up/fetchAll/${page}/${size}`);
         console.log(response.status);
-        setDailyUpList(response.data);
-        // setTotalPages(response.data.page.totalPages);
+        setDailyUpList(response.data._embedded.promoList);
+        console.log(response.data._embedded.promoList);
+        setTotalPages(response.data.page.totalPages);
       } catch (error) {
         console.error("Error fetching Daily Updates", error);
       }
@@ -47,8 +48,7 @@ const DailyUpPage = ({formatDate}) => {
           <p>Daily Updates 가 없습니다.</p>
         )}
       </div>
-      {/* <PageController page={page} setPage={setPage} totalPages={totalPages}/> */}
-      <button className='upload-daily-up' onClick={() => nav('/daily-up/upload')}>Daily Updates 작성하기</button>
+      <PageController page={page} setPage={setPage} totalPages={totalPages}/>
     </div>
   );
 
