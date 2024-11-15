0618d88fe4a6f60fb64a8a287f9611d22bac0773
GK <weeeeek2@naver.com>
2024-11-15 16:58:33 +0900

    fix: Add PagedModel for Daily Update

--- src/main/java/com/teame/promo/PromoController.java
+++ src/main/java/com/teame/promo/PromoController.java
@@ -2,8 +2,16 @@ package com.teame.promo;
 
 import com.teame.club.Club;
 import com.teame.club.ClubDTO;
+import com.teame.club.comment.QnA;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,16 +36,30 @@ public class PromoController {
         this.promoDataParser = promoDataParser;
     }
 
-    @GetMapping("/api/daily-up/fetchAll")
-    public List<PromoDTO> fetchAllPromos() {
-        List<Promo> promos = promoRepository.findAll();
-        return promos.stream()
-                .map(promoService::setPromoDTO)
-                .collect(Collectors.toList());
-    }
+//    @GetMapping("/api/daily-up/fetchAll")
+//    public List<PromoDTO> fetchAllPromos() {
+//        List<Promo> promos = promoRepository.findAll();
+//        return promos.stream()
+//                .map(promoService::setPromoDTO)
+//                .collect(Collectors.toList());
+//    }
     @GetMapping("/api/daily-up/fetchById/{promoId}")
     public ResponseEntity<?> fetchPromoByIdAPI(@PathVariable Long promoId) {
         return promoService.fetchPromoById(promoId);
+    }
+
+    @GetMapping("/api/daily-up/fetchAll/{page}/{size}")
+    public ResponseEntity<?> fetchAllPromos(@PathVariable int page,
+                                         @PathVariable int size,
+                                         PagedResourcesAssembler<Promo> assembler) {
+        try {
+            Pageable pageable = PageRequest.of(page, size, Sort.by("postedAt").descending());
+            Page<Promo> promoPage = promoRepository.findAll(pageable);
+            PagedModel<EntityModel<Promo>> pagedModel = assembler.toModel(promoPage);
+            return ResponseEntity.status(HttpStatus.OK).body(pagedModel);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Promo 불러오기 실패: " + e.getMessage());
+        }
     }
 
 
