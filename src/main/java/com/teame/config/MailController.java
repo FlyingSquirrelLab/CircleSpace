de4bf697cb7df297a02f0efac3848450c702cba8
Johnny Bae <theobsh@gmail.com>
2024-11-20 15:21:43 +0900

    feat : email verification, university identification added

--- src/main/java/com/teame/config/MailController.java
+++ src/main/java/com/teame/config/MailController.java
@@ -2,6 +2,7 @@ package com.teame.config;
 
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,9 @@ public class MailController {
 
   private final MailService mailService;
 
-  @PostMapping("/api/emailCheck")
-  public String emailCheck(@RequestBody MailDTO mailDTO) throws MessagingException {
-    String authCode = mailService.sendSimpleMessage(mailDTO.getEmail());
-    return authCode;
+  @PostMapping("/api/register/sendEmail/{username}")
+  public String emailCheck(@PathVariable String username) throws MessagingException {
+    return mailService.sendSimpleMessage(username);
   }
 
 }
--- src/main/java/com/teame/member/register/RegisterController.java
+++ src/main/java/com/teame/member/register/RegisterController.java
@@ -1,13 +1,16 @@
 package com.teame.member.register;
 
+import com.teame.club.university.University;
+import com.teame.club.university.UniversityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Log4j2
 @RestController
@@ -15,6 +18,7 @@ import java.util.Map;
 public class RegisterController {
 
   private final RegisterService registerService;
+  private final UniversityRepository universityRepository;
 
   @PostMapping("/api/register/phoneNumberCheck")
   public ResponseEntity<String> phoneNumberCheckAPI(@RequestBody String request) {
@@ -29,6 +33,20 @@ public class RegisterController {
   @PostMapping("/api/register/displayNameCheck")
   public ResponseEntity<String> displayNameCheckAPI(@RequestBody String request) {
     return registerService.displayNameCheck(request);
+  }
+
+  @GetMapping("/api/register/universityCheck/{username}")
+  public ResponseEntity<String> universityCheckAPI(@PathVariable String username) {
+    String domain = username.substring(username.indexOf('@') + 1);
+
+    Optional<University> universityOpt = universityRepository.findByCode(domain);
+
+    if (universityOpt.isPresent()) {
+      String universityName = universityOpt.get().getTitle();
+      return ResponseEntity.status(HttpStatus.OK).body(universityName + " 소속으로 확인되었습니다.");
+    } else {
+      return ResponseEntity.status(HttpStatus.OK).body("해당 이메일 도메인으로는 소속대학을 확인할 수 없습니다.");
+    }
   }
 
   @PostMapping("/api/register/registerProc")
