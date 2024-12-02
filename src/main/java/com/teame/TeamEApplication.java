d7ebd48f8a59c20278079b04881e15970edec2e1
GK <weeeeek2@naver.com>
2024-12-03 07:14:48 +0900

    fix: Update default timezone to Asia/Seoul

--- src/main/java/com/teame/TeamEApplication.java
+++ src/main/java/com/teame/TeamEApplication.java
@@ -1,8 +1,11 @@
 package com.teame;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,8 @@ public class TeamEApplication {
 		SpringApplication.run(TeamEApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setDefaultTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
